b27efa30194e45b8922542e00929b22f54e4764b
Jonasta dos SAntos <jonastads@gmail.com>
2018-01-30 18:59:32 -0200

    Update memoria.js

--- memoria.js
+++ memoria.js
@@ -13,24 +13,3 @@ unstage
 	
 ignore mudanças
 	git checkout -q -- src/app/cliente/cliente.service.ts
-	
-	
-	
-	
-Dopamina
-	Dividir os grandes objetivos em pequenos pedaços, é crucial realmente celebrar cada etapa
-
-Serotonina
-	Refletir sobre sucessos significativos do passado 
-	 se expor ao sol
-
-Oxitocina
-	dar um abraço em alguém (oito abraços por dia!)
-	níveis de oxitocina das pessoas aumentam 22% quando a pessoa canta.
-
-Endorfinas
-	chocolate amargo 
-
-gerais
-	exercicio
-	meditação 		
